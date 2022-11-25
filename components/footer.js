f5bb1ecf475c98494a28cb1f0d23a44b0ac09259
soga.johnny <soga.johnny@gmail.com>
2022-11-26 04:59:03 +0900

    add components footer

---
+++ components/footer.js
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export function Footer(props) {
+    console.log(props);
+    return (
+<footer className={styles.footer}>
+<a
+  href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Powered{props.title} by{' '}
+  <span className={styles.logo}>
+    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+  </span>
+</a>
+</footer>
+  )
+}
